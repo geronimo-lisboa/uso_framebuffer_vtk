e4c0493dca024b5a736dfa9926688f937f5e4d47
luciano <luciano.geronimo.fnord@gmail.com>
2017-10-04 16:32:38 -0300

    Tem que botar a luz.

--- myLightningTestActor.cpp
+++ myLightningTestActor.cpp
@@ -18,6 +18,8 @@
 #include <vtkPointData.h>
 #include <vtkDataArray.h>
 #include <vtkPoints.h>
+#include <vtkLightCollection.h>
+#include <vtkLight.h>
 
 
 vtkObjectFactoryNewMacro(myLightiningTestActor);
@@ -174,22 +176,27 @@ int myLightiningTestActor::RenderOpaqueGeometry(vtkViewport *view) {
 
 		glBindVertexArray(vao);//Começa a usar o vartex array
 		shader->UseProgram();//Começa a usar o shader
-		GLuint positionLocation = shader->GetAttribute("position");//pega a localização da vertex position e faz o bind com o vao
+		//Passa os atributos do vao, como ligados na construção do objeto.
+		GLuint positionLocation = shader->GetAttribute("position");
 		glBindAttribLocation(shader->GetProgramId(), positionLocation, "position");
-		GLuint textureCoordinateLocation = shader->GetAttribute("textureCoordinate");//pega a localização da vertex position e faz o bind com o vao
+		GLuint textureCoordinateLocation = shader->GetAttribute("textureCoordinate");
 		glBindAttribLocation(shader->GetProgramId(), textureCoordinateLocation, "textureCoordinate");
-		GLuint normalLocation = shader->GetAttribute("normal");//pega a localização da vertex position e faz o bind com o vao
+		GLuint normalLocation = shader->GetAttribute("normal");
 		glBindAttribLocation(shader->GetProgramId(), normalLocation, "normal");
+		//passa as matrizes
 		GLuint modelMatrixLocation = shader->GetUniform("modelMatrix");
 		glUniformMatrix4fv(modelMatrixLocation, 1, true, modelData.data());
 		GLuint viewMatrixLocation = shader->GetUniform("viewMatrix");
 		glUniformMatrix4fv(viewMatrixLocation, 1, true, viewData.data());
 		GLuint projectionMatrixLocation = shader->GetUniform("projectionMatrix");
 		glUniformMatrix4fv(projectionMatrixLocation, 1, true, projData.data());
+		//passa a luz
+		array<float, 3> colorPos;
+		ren->GetLights()->GetNumberOfItems
+		//Passa as texturas.
 		glActiveTexture(GL_TEXTURE0);
 		GLuint textureLocation = shader->GetUniform("texUnit");
 		glUniform1i(textureLocation, /*GL_TEXTURE*/0);
-
 		glBindTexture(GL_TEXTURE_2D, textureId);
 
 		glDrawArrays(GL_TRIANGLES, 0, vertexes.size());
--- shaders/lightining.fs
+++ shaders/lightining.fs
@@ -18,26 +18,26 @@ void main() {
     //O mesmo vale para a normal. Ela também é interpolada e tb tem que estar no sist
     //de coordenadas global
     
-	//vec3 fragmentWorldPosition = (modelMatrix * vec4 (vPosition,1.0)).xyz;
-    //vec3 normalWorldPosition = normalize( vec3( modelMatrix * vec4( vNormal, 0.0 ) ) );
+	vec3 fragmentWorldPosition = (modelMatrix * vec4 (vPosition,1.0)).xyz;
+    vec3 normalWorldPosition = normalize( vec3( modelMatrix * vec4( vNormal, 0.0 ) ) );
 	
 	//Agora o vetor da origem da luz para o ponto do fragmento.
 	
-    //vec3 lightVector = normalize( lightPosition - fragmentWorldPosition );
+    vec3 lightVector = normalize( lightPosition - fragmentWorldPosition );
 	
     //O brilho da superfície será o produto escalar entre a normal e o vetor entre
     //o ponto da luz e o ponto do fragmento. O prod.escalar, para vetores normalizados,
     //e todos os vetores aqui estão normalizados, está entre [-1,1].
 	
-    //float brightness = dot( normalWorldPosition, lightVector );
+    float brightness = dot( normalWorldPosition, lightVector );
 	
     //Multiplica a cor (que poderia estar vindo de um sampler) pelo brilho para que
     //a cor varie de acordo com a posição do fragmento em relação à camera. A imagem 
     //da brightness está no intervalo [-1,1] e qqer coisa <= 0 zera a cor de saida.
 
 	vec4 texColor = texture(texUnit, vTextureCoordinate);
-	//texColor.x = texColor.x * brightness;
-	//texColor.y = texColor.y * brightness;
-	//texColor.z = texColor.z * brightness;
-    frag_colour = vec4(vTextureCoordinate.x,vTextureCoordinate.y,vNormal.x,texColor.x);
+	texColor.x = texColor.x * brightness;
+	texColor.y = texColor.y * brightness;
+	texColor.z = texColor.z * brightness;
+    frag_colour = texColor;
 }
