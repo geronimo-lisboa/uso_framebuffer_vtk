3d8f890497c404385b329899b79ce1f7862db1a3
luciano <luciano.geronimo.fnord@gmail.com>
2017-10-04 12:42:05 -0300

    A partir daqui começo a fazer a iluminação.

---
+++ shaders/lightining.fs
@@ -0,0 +1,33 @@
+#version 400
+uniform sampler2D texture;
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+uniform vec3 lightPosition;
+
+in vec3 vPosition;
+in vec3 vNormal;
+in vec2 vTextureCoordinate;
+
+out vec4 frag_colour;
+
+void main() {
+void main() {
+    //A posição do vertice responsável pelo fragmento é interpolada, e essa interpolação
+    //está guardada em vPosition. vPosition é a posição do fragmento atual em Object
+    //Coordinates. Logo, multiplicar pela ModelMatrix tornará ela World Coordinate.
+    //O mesmo vale para a normal. Ela também é interpolada e tb tem que estar no sist
+    //de coordenadas global
+    vec3 fragmentWorldPosition = (modelMatrix * vec4 (vPosition,1.0)).xyz;
+    vec3 normalWorldPosition = normalize( vec3( modelMatrix * vec4( vNormal, 0.0 ) ) );
+	//Agora o vetor da origem da luz para o ponto do fragmento.
+    vec3 lightVector = normalize( lightPosition - fragmentWorldPosition );
+    //O brilho da superfície será o produto escalar entre a normal e o vetor entre
+    //o ponto da luz e o ponto do fragmento. O prod.escalar, para vetores normalizados,
+    //e todos os vetores aqui estão normalizados, está entre [-1,1].
+    float brightness = dot( normalWorldPosition, lightVector );
+    //Multiplica a cor (que poderia estar vindo de um sampler) pelo brilho para que
+    //a cor varie de acordo com a posição do fragmento em relação à camera. A imagem 
+    //da brightness está no intervalo [-1,1] e qqer coisa <= 0 zera a cor de saida.
+    gl_FragColor = vec4( color * brightness, 1.0 );
+}
---
+++ shaders/lightining.vs
@@ -0,0 +1,19 @@
+#version 400
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+
+in vec3 position;
+in vec2 textureCoordinate;
+in vec3 normal;
+
+out vec3 vPosition;
+out vec3 vNormal;
+out vec2 vTextureCoordinate;
+
+void main(){
+	vTextureCoordinate = textureCoordinate;
+	vPosition = position;
+	vNormal = normal;
+	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
